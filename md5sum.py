68a4a145586c025046e750e5b0473a70588a8cca
adam <adam@monkeez.org>
2020-12-12 10:16:37 +0000

    We have a working copy that prints out md5sums

--- md5sum.py
+++ md5sum.py
@@ -1,8 +1,17 @@
-import os
+from pathlib import Path
+import hashlib
 
-for filename in os.listdir(directory):
-    if filename.endswith(".txt"):
-         print(os.path.join(directory, filename))
-        continue
-    else:
-        continue
+pathlist = Path("test").glob('**/*.txt')
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+for path in pathlist:
+     hash = md5(path)
+     print(path, ": ", hash)
+     # TODO output file name and hash on one line to a file
+
