19f8c52c1e52ae97d5e8b15eb2082fdc87195a7b
adam <adam@monkeez.org>
2020-12-15 19:48:23 +0000

    We now check if md5sum exists remotely. If not, we write that to a log file

--- md5sum.py
+++ md5sum.py
@@ -4,6 +4,7 @@ import os
 from azure.storage.blob import BlobServiceClient
 from os import sys
 import base64
+import binascii
 
 def md5(fname):
     hash_md5 = hashlib.md5()
@@ -13,50 +14,49 @@ def md5(fname):
     return hash_md5.hexdigest()
 
 
-def remote_check(connection_str):
+def remote_md5_check(connection_str, file):
     blob_service_client = BlobServiceClient.from_connection_string(connection_str)
     container_name = "global"
     container = blob_service_client.get_container_client(container=container_name)
 
     blob_list = container.list_blobs()
-    count = 0
     for blob in blob_list:
-        if count < 1:
-            blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob)
-            a = blob_client.get_blob_properties()
-            # print(a.metadata.keys())
-            b = (a.content_settings.content_md5)
-            print(b)
-            # print (base64.b64encode(b.encode('utf-8')))
-            for i in range(len(b)):
-                print("B is :" + str(b[i]))
-            print("B is type: " + str(type(b[0])))
-            # print("B decoded" + b.decode('utf8'))
-            print("Blob name: " + str(blob_client.blob_name ))
-            # print(blob.get_blob_properties(container_name, blob.name))
-            count = count + 1
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob)
+        a = blob_client.get_blob_properties()
+        print("Name: " + a.name)
+        b = a.content_settings.content_md5
+        if b != None:
+            print("B: " + str(b))
+            remote_md5 = binascii.hexlify(b)
+            output = a.name.split('/')[-1] + ": " + str(remote_md5.decode("utf-8")) + "\n"
+            print(output)
         else:
-           break
+            output = a.name.split('/')[-1] + " Did not have an md5sum." + "\n"
+            print(output)
+        file.write(output)
+
 
 def local_md5_check(pathlist, file):
     for path in pathlist:
         # As I can get the md5sum straight from Azure - I don't need this weird hex base64 thing.
         # command = "md5sum --binary " + str(path.absolute()) +" | awk '{print $1}' | xxd -p -r | base64"
-        # name = str(path.name)
+        name = str(path.name)
         hash_calc = md5(path)
-        print(path.name, hash_calc)
+        # This output used the old version which issued a bash command:
         # output = name + ": " + os.popen(command).read()
-        # print (output)
+        # This output outputs the pure python method
+        output = name + ": " + hash_calc +"\n"
+        print (output)
         file.write(output)
 
-def main():
-    file = open("md5_local_jpeg.txt", "w")
-    # The below path is for windows:
-    # pathList = Path("C:/Users/adamc/jewson-images/global/product-images").glob('**/*.jpg')
-    # This path is for linux:
-    pathlist = Path("/mnt/c/Users/adamc/jewson-images/global/product-images/").glob('**/*.jpg')
-    local_md5_check(pathlist, file)
-    file.close()
+# def main():
+#     file = open("md5_local_jpeg.txt", "w")
+#     # The below path is for windows:
+#     # pathList = Path("C:/Users/adamc/jewson-images/global/product-images").glob('**/*.jpg')
+#     # This path is for linux:
+#     pathlist = Path("/mnt/c/Users/adamc/jewson-images/global/product-images/").glob('**/*.jpg')
+#     local_md5_check(pathlist, file)
+#     file.close()
 
 
 # def main():
@@ -67,13 +67,15 @@ def main():
 #     file.close()
 
 
-# def main():
-#     try:
-#         CONNECTION_STRING = os.environ['AZURE_STORAGE_CONNECTION_STRING']
-#         remote_check(CONNECTION_STRING)
-#     except KeyError:
-#         print("AZURE_STORAGE_CONNECTION_STRING must be set.")
-#         sys.exit(1)
+def main():
+    try:
+        CONNECTION_STRING = os.environ['AZURE_STORAGE_CONNECTION_STRING']
+        file = open("md5_remote.txt", "w")
+        remote_md5_check(CONNECTION_STRING, file)
+        file.close()
+    except KeyError:
+        print("AZURE_STORAGE_CONNECTION_STRING must be set.")
+        sys.exit(1)
 
 if __name__ == '__main__':
     main()
